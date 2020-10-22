1dbe2eaebb991fcb00269c02497cd907d1c4a22c
Allvaa <alvamailindo@gmail.com>
2020-10-22 17:08:37 +0700

    feat: typing

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+declare interface Song {
+    title: string;
+    image: string;
+    geniusUrl: string;
+    lyrics: string;
+}
+
+declare const getLyrics: {
+    (query: string): Promise<Song>;
+}
+
+export = getLyrics;
