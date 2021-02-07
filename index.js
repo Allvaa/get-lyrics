af9685f729e8e8844fcf7e1fa22ca064ad415ee4
Allvaa <52526068+Allvaa@users.noreply.github.com>
2021-02-07 13:11:13 +0700

    set request user agent (#2)

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const { load } = require("cheerio");
 
 const baseURL = "https://genius.com/api";
 
+const reqOpt = {
+    headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+    }
+};
+
 const search = (query) => {
     return new Promise((resolve, reject) => {
-        fetch(`${baseURL}/search/song?q=${encodeURIComponent(query)}`)
+        fetch(`${baseURL}/search/song?q=${encodeURIComponent(query)}`, reqOpt)
           .then(res => res.json())
           .then(body => body.response.sections[0].hits.length ? body.response.sections[0].hits[0].result : undefined)
           .then(resolve)
@@ -17,7 +23,7 @@ const getLyrics = async (query) => {
     if (!query || typeof query !== "string") return;
     const song = await search(query);
     if (!song) return;
-    const html = await (await fetch(song.url)).text();
+    const html = await (await fetch(song.url, reqOpt)).text();
     const lyrics = load(html)(".lyrics").text().trim();
     return {
         title: song.full_title,
